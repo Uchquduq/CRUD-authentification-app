ae8384fc196f8a9090be983e90863bc9ecd97a7b
Rufat <rufatxasanov1998@gmail.com>
2022-07-03 22:53:52 +0500

    ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
-  <CrudTable v-if="isLoggedIn" />
+  <div>
+    <Login v-if="isAnonymous" />
+    <CrudTable v-if="isLoggedIn" />
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import { getterTypes } from "@/store/modules/auth";
 import CrudTable from "../components/CrudTable";
+import Login from "@/views/Login";
 
 export default {
   name: "Home",
   components: {
     CrudTable,
+    Login,
   },
   computed: {
     ...mapGetters({
